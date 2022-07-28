640679550cc2286ab1aae142e6fa7d631e2de8a6
Elif <elif@PC.local>
2022-07-29 00:07:31 +0300

    Add EC2 Instance configuration

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
 # Basic AWS CDK Project
-In this project, you'll find basic configuration for VPC and EC2 services that written in typescript language with cdk.
-1. VPC Configuration
+In this project, you'll find basic configuration for VPC and EC2 services that written in typescript language with cdk
+## 1. VPC Configuration
 ```typescript
-  vpcName: "Vpc-01",
+      vpcName: "Vpc-01",
       cidr: "10.0.0.0/16",
       subnetConfiguration: [
         {
           cidrMask: 24,
           name: 'Ingress',
-          subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
+          subnetType: aws_ec2.SubnetType.PUBLIC,
         }
       ],
-      maxAzs: 2
+      maxAzs: 1
 ```
-* 16 cidr block for vpc and 24 cidrMask for subnets,
-* Subnet type as 'PRIVATE_ISOLATED' which means VPC is private and it doesn't have any internet gateway(igw) for connecting to internet,
-* The count of subnet networks maximum availability zones is 2; eu-central-1a and eu-central-1b
+* 16 cidr block for VPC and 24 cidrMask for VPC-Subnet
+* Subnet type as 'PUBLIC' which means VPC has internet gateway(igw) for connecting to internet
+* The count of subnet networks maximum availability zones is 1
+## 2. EC2 Instance Configuration
+
+```typescript
+      instanceName: 'machine-01',
+      vpc,
+      machineImage: machineImage,
+      instanceType: new aws_ec2.InstanceType('t2.micro'),
+      securityGroup: instanceSg,
+      keyName: 'machine-01',
+```
+* The used Machine Image is Ubuntu
+* The Instance Type as t2.micro which is acceptable for basic processes
+* The Security Group is defined for ssh, http, https connections
+* The Key-Pair is created via aws console for ssh connection with local pc
+
+
 
--- lib/basic-project-stack.ts
+++ lib/basic-project-stack.ts
@@ -2,24 +2,50 @@ import { aws_ec2, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Ec2Action } from 'aws-cdk-lib/aws-cloudwatch-actions';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BasicProjectStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const Vpc = new aws_ec2.Vpc(this, "Vpc", {
+    //Create VPC with private_isolated subnets
+    const vpc = new aws_ec2.Vpc(this, "Vpc", {
       vpcName: "Vpc-01",
       cidr: "10.0.0.0/16",
       subnetConfiguration: [
         {
           cidrMask: 24,
           name: 'Ingress',
-          subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
+          subnetType: aws_ec2.SubnetType.PUBLIC,
         }
       ],
-      maxAzs: 2
-    })
-    
+      maxAzs: 1
+    });
+
+    //Create security group for instance
+    const instanceSg = new aws_ec2.SecurityGroup(this, 'Instance-SG', {
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    instanceSg.addIngressRule(aws_ec2.Peer.anyIpv4(), aws_ec2.Port.tcp(22), 'allow ssh from anywhere');
+    instanceSg.addIngressRule(aws_ec2.Peer.anyIpv4(), aws_ec2.Port.tcp(80), 'allow http traffic from anywhere');
+    instanceSg.addIngressRule(aws_ec2.Peer.anyIpv4(), aws_ec2.Port.tcp(443), 'allow https traffic from anywhere');
+
+
+    //Select ubuntu image with ami id(taken from ubuntu ami calculator)
+    const machineImage = aws_ec2.MachineImage.genericLinux({
+      'eu-central-1': 'ami-065deacbcaac64cf2',
+    });
+
+    //Create instance t2.micro type with ubuntu os
+    const ec2Instance = new aws_ec2.Instance(this, 'Instance-01', {
+      instanceName: 'machine-01',
+      vpc,
+      machineImage: machineImage,
+      instanceType: new aws_ec2.InstanceType('t2.micro'),
+      securityGroup: instanceSg,
+      keyName: 'machine-01',
+    });
+
   }
 }
