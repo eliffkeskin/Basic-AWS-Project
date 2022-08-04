2a047d4ed359d09d7785583bc2ba0810979c8746
Elif <elif@PC.local>
2022-08-04 23:01:30 +0300

    Modify basic-project stack with adding new services in bin folder.

--- bin/basic-project.ts
+++ bin/basic-project.ts
@@ -2,6 +2,19 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { BasicProjectStack } from '../lib/basic-project-stack';
+import { getConfig } from '../lib/config';
+import { VPCStack } from '../lib/vpc';
+import { EC2Stack } from '../lib/ec2/ec2-stack';
+import { ECRStack } from '../lib/ecr';
 
 const app = new cdk.App();
-new BasicProjectStack(app, 'BasicProjectStack', {});
+const conf = getConfig(app);
+const env = {
+    account: conf.account,
+    region: conf.region,
+};
+
+//new BasicProjectStack(app, 'BasicProjectStack', {});
+new VPCStack(app, 'VPCStack', { env });
+new EC2Stack (app, "EC2Stack", { env });
+new ECRStack(app, "ECRStack", { env });
